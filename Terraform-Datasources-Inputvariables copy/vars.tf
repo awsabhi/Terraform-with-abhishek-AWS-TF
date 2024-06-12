d0f80f948c8e80a2e5878eae7fee83b2172c7779
abhishek <abhishekmishra0791@gmail.com>
2024-06-12 23:21:48 +0530

    1st commit for this Block

---
+++ Terraform-Datasources-Inputvariables copy/vars.tf
@@ -0,0 +1,19 @@
+variable "AWS_ACCESS_KEY" {
+    type = string
+  
+}
+
+variable "AWS_SECRET_KEY" {
+    type = string
+  
+}
+
+variable "aws_region" {
+    type = string
+    default = "us-east-1"
+}
+# variable "instance_type" {
+#     type = string
+#     default = "t2.micro"
+  
+# }
