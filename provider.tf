8aab491f1e851e03f74f6cfb232dbb0bc5420ece
abhishek <abhishekmishra0791@gmail.com>
2024-06-12 22:47:41 +0530

    Create the seperate file for resources

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.53.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+}
+
+# resource "aws_instance" "demo-vpc" {
+#     ami = "ami-0b0ea68c435eb488d"
+#     instance_type = "t2.micro"
+  
+# }
